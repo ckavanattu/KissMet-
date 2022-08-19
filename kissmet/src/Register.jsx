7fc72681dddcfa89a232a758f619084dd7430dc2
Anuka <anukazan@gmail.com>
2022-08-18 18:49:14 -0700

    minor edits to text

--- kissmet/src/Register.jsx
+++ kissmet/src/Register.jsx
@@ -15,12 +15,12 @@ export const Register = (props) => {
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
             <label htmlFor="name">Full name</label>
-            <input value={name} name="name" id="name" placeholder="full Name" />
+            <input value={name} name="name" id="name" placeholder="Full Name" />
             <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="Wmail" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button type="submit">Log In</button>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="Password" placeholder="********" id="password" name="password" />
+            <button type="submit"> Sign Me Up! </button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
