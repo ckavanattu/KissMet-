6bc9c9834ca5fec81f04d0b52024529eacb1a482
hvphan1993 <hvphan1993@gmail.com>
2022-08-21 20:22:10 -0700

    trying to add features

--- client/src/pages/CreateProfile.jsx
+++ client/src/pages/CreateProfile.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import Auth from '../utils/auth';
 import { Link } from "react-router-dom";
 
+
 export const CreateProfile = () => {
+
+
 
     return (
         <div className="App">
-            "Work in progress!"
+            <h1>Welcome {name} </h1>
         </div>
     )
 
